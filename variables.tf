b0dd33dad36a7e408f0fbd3cc4273f8f3809d999
Mohamed Yasser <Mohamed.Yasser@Khazyn.app>
2024-09-11 10:35:40 +0300

    ec2 with key

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "key-name" {
+
+}
+
+variable "network-security-group-name" {
+
+}
+
+variable "ubuntu-ami" {
+
+}
+
+variable "ubuntu-instance-type" {
+
+}
+
+variable "region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = ""
+}
